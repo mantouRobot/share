d57003f1fce6ac37b90735b4b72738b96a8d3bd1
jian.song <jian.song@nxrobo.com>
2016-10-23 16:19:44 +0800

    test actionlib,参考frontier exploration和move_base，最终实现嵌套，用并行的思维去理解actionlib抢占行为，取消行为，多次判断状态，最后设置状态

--- seven_ws/src/pkg_test_actionlib/src/client_node.cpp
+++ seven_ws/src/pkg_test_actionlib/src/client_node.cpp
@@ -1,20 +1,38 @@
 #include <ros/ros.h>
+#include <std_msgs/Int32.h>
+#include <pkg_test_actionlib/Cout.h>
+
 #include <actionlib/client/simple_action_client.h>
 #include <pkg_test_actionlib/CoutAction.h>
 
 class CoutActionClient
 {
 public:
+  ros::NodeHandle nh_;
+  ros::ServiceServer sub_;
   actionlib::SimpleActionClient<pkg_test_actionlib::CoutAction> ac_;
   CoutActionClient() :
     ac_("server_node", true)//true: 不需要spin,有线程在spin
   {
-    ac_.waitForServer(ros::Duration(1));
+    ROS_DEBUG_NAMED("test_actionlib", "%s", "hello world");//for console
+    sub_ = nh_.advertiseService("/s_buildmap", &CoutActionClient::srvCb, this);
+    ac_.waitForServer(ros::Duration(5));
+  }
+
+  bool srvCb(pkg_test_actionlib::Cout::Request &req, pkg_test_actionlib::Cout::Response &res)
+  {
     pkg_test_actionlib::CoutGoal goal;
-    goal.times = 5;
-    ac_.sendGoal(goal);
-    ac_.waitForResult(ros::Duration(30));
-    ac_.cancelAllGoals();
+    goal.times = req.times;
+    if(goal.times == 0)
+    {
+      ac_.cancelAllGoals();
+      res.result = false;
+    }
+    else
+    {
+      ac_.sendGoal(goal);
+      res.result = true;
+    }
   }
 };
 
--- seven_ws/src/pkg_test_actionlib/src/server_node.cpp
+++ seven_ws/src/pkg_test_actionlib/src/server_node.cpp
@@ -1,5 +1,6 @@
 #include <ros/ros.h>
 #include <actionlib/server/simple_action_server.h>
+#include <actionlib/client/simple_action_client.h>
 #include <pkg_test_actionlib/CoutAction.h>
 
 class CoutActionServer
@@ -7,34 +8,66 @@ class CoutActionServer
 public:
   ros::NodeHandle nh_;
   actionlib::SimpleActionServer<pkg_test_actionlib::CoutAction> as_;
+  actionlib::SimpleActionClient<pkg_test_actionlib::CoutAction> embed_ac_;
+  boost::mutex embed_ac_lock_;
+
   pkg_test_actionlib::CoutGoal goal_;
   pkg_test_actionlib::CoutResult result_;
 
   CoutActionServer(ros::NodeHandle nh) :
     nh_(nh),
+    embed_ac_("embed_server", true),
     as_(nh_, "server_node", boost::bind(&CoutActionServer::executeCB, this, _1), false)//bool: auto start
   {
+    embed_ac_.waitForServer(ros::Duration(5));
+    as_.registerPreemptCallback(boost::bind(&CoutActionServer::preemptCb, this));
     as_.start();
   }
 
+  /**
+   * @brief preemptCb 又一个客户请求则进入抢占回调。
+   *                  在抢占回调里应该实现：
+   *                    1. 取消本服务里的其他可能在执行的内层服务
+   *                    2. 将as_设置为抢占即可结束当前服务
+   *                    3. 如有需要，发布上一服务的结果（逻辑上为未完成）或此时的中间反馈值
+   */
+  void preemptCb()
+  {
+    ROS_ERROR("New goal will preempt and execute. Old goal will aborted!");
+    boost::unique_lock<boost::mutex> lock(embed_ac_lock_);//生命结束，自动解锁
+    embed_ac_.cancelGoalsAtAndBeforeTime(ros::Time::now());
+    if(as_.isActive())
+    {
+      as_.setPreempted();//抢占
+    }
+  }
+
+  /**
+   * @brief executeCB 服务的执行函数。
+   * @param goal
+   */
   void executeCB(const pkg_test_actionlib::CoutGoalConstPtr &goal)//一个const都不能少
   {
     ros::Rate r(1);
     int tmp = goal->times;
-    while(ros::ok())
+    while(ros::ok() && as_.isActive())
     {
-      if(as_.isPreemptRequested() ||  !ros::ok())
-      {
-        ROS_ERROR("Preempted!");
-        as_.setPreempted();
-        result_.result = false;
-        break;
-      }
+      pkg_test_actionlib::CoutGoal embed_goal;
+      embed_goal.times = 5;
+      embed_ac_.sendGoal(embed_goal);
+      embed_ac_.waitForResult(ros::Duration(10));
       tmp++;
       ROS_INFO("Cout action: %d", tmp);
+      if(as_.isActive() && tmp == 8)
+      {
+        as_.setSucceeded();
+        break;
+      }
       r.sleep();
     }
 //    as_.setSucceeded();
+//    as_.setPreempted();
+//    as_.setAborted();
   }
 };
 
